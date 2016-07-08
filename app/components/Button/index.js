f27e412d3db2b6fe9c50e6c5918677fbd011bb6e
Adam <ads1018@gmail.com>
2016-07-08 10:11:38 -0400

    Remove console.log

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import styles from './Button.scss';
 
-console.log(styles);
 export default function Button(props) {
   return (
     <div className={styles.container}>
